8bf5565682fa2d155a65f3b9beee27cf4f24991d
RyanBeevers <43890900+RyanBeevers@users.noreply.github.com>
2019-05-07 10:30:28 -0400

    Update InvoicesRepo.java

--- tbb/src/main/java/com/tbb/repos/InvoicesRepo.java
+++ tbb/src/main/java/com/tbb/repos/InvoicesRepo.java
@@ -1,16 +1,17 @@
-package com.tbb.repos;
+package com.example.demo.Repos;
 
 import java.util.List;
 
+import com.example.demo.models.Invoices;
+import com.example.demo.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.tbb.models.Invoices;
-import com.tbb.models.Users;
 
 @Repository
 public interface InvoicesRepo extends JpaRepository <Invoices, Integer>{
 
-	List<Invoices> findAllByUser(Users user);
-	
+    List<Invoices> findAllByUser(Users user);
+
+    Invoices findInvoicesByInvoiceId(Integer invoiceId);
+
 }
